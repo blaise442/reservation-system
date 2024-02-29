5025d336f06edc7ecb1b56feb2833ec588b833f1
Blaze N <2487858872az@gmail.com>
2024-02-28 19:29:32 -0600

    adding initial files for collab

---
+++ frontend/src/components/pages/GDPR.tsx
@@ -0,0 +1,170 @@
+// STYLED COMPONENTS //
+import styled from "styled-components";
+import { devices } from "../styling-breakpoints/breakpoints/Breakpoints";
+import {
+  StyledMediumHeading,
+  StyledSmallHeading,
+} from "../styled-components/Headings/StyledHeadings";
+import { StyledParagraph } from "../styled-components/Text/StyledParagraph";
+import { StyledFlexDiv } from "../styled-components/Wrappers/StyledFlex";
+
+export default function GDPR() {
+  window.scrollTo(0, 0);
+
+  return (
+    <GDPRWrapper>
+      <StyledFlexDiv justify="flex-start" align="flex-start">
+        <StyledMediumHeading textAlign="left">GDPR</StyledMediumHeading>
+        <StyledSmallHeading padding="10px 0px">
+          Handling of personal data
+        </StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          At jagsspace we treat Personal data for users and guests using jagsspace
+          online booking system.. We safeguard your privacy and follow the
+          applicable legislation at any time, which aims to protect you as a
+          private person. The restaurant is the personal data controller for the
+          processing of personal data on this website. Information on how to
+          contact us is available under the link Contact at the top of the page.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          By “personal data” is meant all information that can be linked
+          directly or indirectly to a live person today.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          This Privacy Policy applies only to users of La Mère's website.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+         jagsspace uses Personal Information primarily to administer, provide,
+          develop and maintain the Services, process your bookings, optimize
+          your experience of the Services and personalize the communication with
+          you. Please read this Privacy Policy carefully to understand how and
+          for what purposes we treat your Personal Information. By submitting
+          your Personal Information to us and accepting the applicable terms,
+          you agree that your Personal Information will be processed in
+          accordance with what is stated in this Privacy Policy. If the law
+          requires a more specific consent from you, we will naturally ask for
+          one.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          This Privacy Policy and La Mère's Terms of Use and any other terms
+          that govern the La Mère's processing of Personal Information indicate
+          the basis for how we treat Personal Information that you provide to us
+          when you use our services or that we collect about you.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          Changes to these now mentioned conditions are announced by the new
+          conditions being published on the La Mère website and we therefore
+          recommend that you regularly take part of these. If you add Personal
+          Information about other persons, you are responsible for ensuring that
+          La Mère may also treat the Personal Data in accordance with applicable
+          conditions.
+        </StyledParagraph>
+        <StyledSmallHeading padding="10px 0px">
+          Use of jag's space website
+        </StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          By booking via La Mère's website you agree that jag's space actively
+          collects, manages, stores and uses, and in other ways treats, your
+          personal data in order for jag's space to be able to provide the services
+          provided via the website.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+        jag's space can disclose Personal Information to third parties, such as
+          police or other authority, whether it concerns investigation of crimes
+          or whether the Theater Hotel is otherwise obliged to disclose such
+          task with the support of law or authority decisions.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          Through this Privacy Policy you also consent to the use of information
+          / registration information in order to identify your target audience
+          for marketing purposes. You also agree that such information may be
+          disclosed to third parties for marketing purposes. Such disclosure of
+          information will always be anonymous. You have the right, after a
+          written application at jag's space, to receive information about which
+          personal data are registered about you. Any inaccurate personal data
+          you may ask to be corrected. You can also apply to have personal data
+          deleted. Such an application is submitted by sending an e-mail to
+          info@jagsspace.
+        </StyledParagraph>
+        <StyledSmallHeading textAlign="left" padding="10px 0px">
+          Objectives for which we treat your personal data
+        </StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          jag's space uses Personal Information to administer, provide, develop and
+          maintain the Services, process your bookings, contact you via eg. SMS
+          or email or to notify your booking status or information that connects
+          to your booking before, during and after your reservation, diagnose
+          errors, optimize the technology and be able to contact you in case of
+          problems with a booking or execution of the Services, analyze and
+          improve the quality and experience of the Services, analyze statistics
+          and user behavior regarding our Services, increase your benefit and
+          experience of our Services in other ways we think you would
+          appreciate, or promote services and products, directly or not,
+          including marketing behavior.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          You are entitled at any time and without charge to request that La
+          Mère not process your Personal Information for marketing purposes or
+          withdraw your consent to this. You can use this right by contacting jag's space at the following address: info@jagsspace.com.
+        </StyledParagraph>
+        <StyledSmallHeading padding="10px 0px">Disclaimer</StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          When you make a reservation with us, we use your personal information
+          to send booking confirmation, important information about the upcoming
+          reservation. Such mailings will be sent to the email address you
+          provided when booking.
+        </StyledParagraph>
+        <StyledSmallHeading padding="10px 0px">
+          Storage times
+        </StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+        jag's space follows good practice in the restaurant industry.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          The restaurant saves all the personal data we collect in our customer
+          database.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          The same personal data can be stored in several different places for
+          different purposes. This may mean that a task that has been culled
+          from a system because it is no longer necessary may remain in another
+          system where it is stored with the support of consent or for another
+          purpose where the personal data is still needed.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          jag's space requires Personal Data in accordance with applicable law at
+          any given time. This means, for example, that La Mère deletes or
+          un-identifies Personal Data when the purpose of the processing of the
+          Personal Data no longer exists.
+        </StyledParagraph>
+        <StyledSmallHeading padding="10px 0px">
+          Technical / organizational measures for safe treatment of personal
+          data
+        </StyledSmallHeading>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          jag's space takes ongoing measures to comply with the principles of
+          “built-in data protection and data protection as standard”. La Mère
+          continuously evaluates the risks associated with the personal data
+          processing that takes place and takes the necessary security measures
+          to reduce the risks.
+        </StyledParagraph>
+        <StyledParagraph textAlign="left" padding="10px 0px" fontSize="1.6rem">
+          Do you have direct questions about how La Mère works with the data
+          protection regulation (GDPR), please contact La Mère.
+        </StyledParagraph>
+      </StyledFlexDiv>
+    </GDPRWrapper>
+  );
+}
+
+export const GDPRWrapper = styled.div`
+  padding: 50px 15px;
+
+  @media ${devices.tablet} {
+    padding: 50px 50px;
+  }
+
+  @media ${devices.desktop} {
+    padding: 100px 200px;
+  }
+`;
